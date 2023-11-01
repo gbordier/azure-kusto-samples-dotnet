f8441e462762033689c5f1d0f7b7efb685fa23c1
Guillaume Bordier <g_bordier@hotmail.com>
2023-11-01 18:32:09 +0000

    added env variable and checks for S3 events

--- S3/S3EventGridPoc/Function.cs
+++ S3/S3EventGridPoc/Function.cs
@@ -30,62 +30,82 @@ public class Function
     /// </summary>
     public Function()
     {
-        const string poolId = "eu-north-1:80ae676e-949e-4ddb-82cd-a0edc8266fc9";
-        const string clientId= "757a0e90-ab7c-42ff-ac1d-8cf7f5d028d6";
-        const string tenantId = "d1d92357-0fd1-4f9d-853e-a2bf3d687614";
+        //const string cognitoPoolId = "eu-north-1:80ae676e-949e-4ddb-82cd-a0edc8266fc9";
+        // const string clientId= "757a0e90-ab7c-42ff-ac1d-8cf7f5d028d6";
+        // const string tenantId = "d1d92357-0fd1-4f9d-853e-a2bf3d687614";
         string region = Environment.GetEnvironmentVariable("AWS_DEFAULT_REGION");
-        Dictionary<string,string>  Logins= new Dictionary<string,string>();
-        Logins["Azure"]="Adx";
-        S3Client = new AmazonS3Client();
-        var cognitoClient = new 	AmazonCognitoIdentityClient( 	 RegionEndpoint.GetBySystemName(region ));
-        var response =  cognitoClient.GetOpenIdTokenForDeveloperIdentityAsync(new GetOpenIdTokenForDeveloperIdentityRequest()
-        {
-            IdentityPoolId = poolId,
-            Logins = Logins
-        }
-        ).Result;
         
+        string cognitoPoolId =Environment.GetEnvironmentVariable("CognitoPoolId");
         string appId = Environment.GetEnvironmentVariable("AppId");
         string appKey = Environment.GetEnvironmentVariable("AppKey");
         string authority = Environment.GetEnvironmentVariable("AppTenant");
         string clusterUri = Environment.GetEnvironmentVariable("IngestionUri");
-        
-        // enumerate environment variables
-        var entries = Environment.GetEnvironmentVariables().Cast<DictionaryEntry>()
-            .Select(x => KeyValuePair.Create((string)x.Key, (string)x.Value));
-        var sortedEntries = entries.OrderBy(x => x.Key);
 
-        foreach ( var  v in sortedEntries)
-        {
-            Console.WriteLine("env var : " + v.Key + " = " + v.Value);
-        }
-        Console.WriteLine($"Initializing an ingest client for {clusterUri}");
-//        Console.WriteLine($"cognito client identity is {cognitoClient.}");
+                // this is debug code very useful to see what is going on
+        if (true){
+            // enumerate environment variables
+            var entries = Environment.GetEnvironmentVariables().Cast<DictionaryEntry>()
+                .Select(x => KeyValuePair.Create((string)x.Key, (string)x.Value));
+            var sortedEntries = entries.OrderBy(x => x.Key);
 
-Console.WriteLine ($" resp {response.IdentityId}");
-Console.WriteLine ($" resp {response.Token}");
-        if (response.Token.Length > 0){    
-            IConfidentialClientApplication app = ConfidentialClientApplicationBuilder.Create(clientId)
-                .WithClientAssertion(response.Token)      
-                .WithAuthority(AzureCloudInstance.AzurePublic, tenantId)          
-                .Build();
-            var token= app.AcquireTokenForClient(new string[] { $"https://management.core.windows.net/.default" }).ExecuteAsync().Result;
-            if (token != null){
-                Console.WriteLine("AAD token is " + token.AccessToken);
+            foreach ( var  v in sortedEntries)
+            {
+                Console.WriteLine("env var : " + v.Key + " = " + v.Value);
             }
         }
-        
-        var kustoConnectionStringBuilderDM =
-            new KustoConnectionStringBuilder(clusterUri)
-            .With
-            .WithAadApplicationKeyAuthentication(appId, appKey, authority);
 
-        Console.WriteLine($"Initializing an ingest client for {clusterUri}");
-        // Create an ingest client
-        // Note, that creating a separate instance per ingestion operation is an anti-pattern.
-        // IngestClient classes are thread-safe and intended for reuse
-        m_client = KustoIngestFactory.CreateQueuedIngestClient(kustoConnectionStringBuilderDM);
-        
+        if (appId != null &&  cognitoPoolId != null) {
+            Console.WriteLine("using cognito");
+            Dictionary<string,string>  Logins= new Dictionary<string,string>();
+            Logins["Azure"]="Adx";
+            S3Client = new AmazonS3Client();
+            var cognitoClient = new 	AmazonCognitoIdentityClient( 	 RegionEndpoint.GetBySystemName(region ));
+            var response =  cognitoClient.GetOpenIdTokenForDeveloperIdentityAsync(new GetOpenIdTokenForDeveloperIdentityRequest()
+            {
+                IdentityPoolId = cognitoPoolId,
+                Logins = Logins
+            }
+            ).Result;
+
+            Console.WriteLine($"Initializing an ingest client for {clusterUri}");
+            //        Console.WriteLine($"cognito client identity is {cognitoClient.}");
+
+            Console.WriteLine ($" got a response from cognito with identity:  {response.IdentityId}");
+            
+            if (response.Token.Length > 0){    
+                Console.WriteLine ($" resp {response.Token}");
+                IConfidentialClientApplication app = ConfidentialClientApplicationBuilder.Create(appId)
+                    .WithClientAssertion(response.Token)      
+                    .WithAuthority(AzureCloudInstance.AzurePublic, authority)          
+                    .Build();
+                var appAccessToken= app.AcquireTokenForClient(new string[] { $"https://management.core.windows.net/.default" }).ExecuteAsync().Result;
+                if (appAccessToken != null){
+                    Console.WriteLine("AAD token is " + appAccessToken.AccessToken);
+                    
+                    Console.WriteLine("creating ingestion client with AAD Token as the authentication") ;               
+                    var kustoConnectionStringBuilderDM = new KustoConnectionStringBuilder(clusterUri)
+                        .WithAadApplicationTokenAuthentication(appAccessToken.AccessToken);
+                    m_client = KustoIngestFactory.CreateQueuedIngestClient(kustoConnectionStringBuilderDM);
+                    Console.WriteLine ("constructed client with conn string:" + kustoConnectionStringBuilderDM.ToString());
+                }
+        }
+        else if (appId != null && appKey != null){
+            Console.WriteLine("using AAD with appKey");
+            var kustoConnectionStringBuilderDM =
+                new KustoConnectionStringBuilder(clusterUri)
+                .WithAadApplicationKeyAuthentication(appId, appKey, authority);
+                        Console.WriteLine($"Initializing an ingest client for {clusterUri}");
+            // Create an ingest client
+            // Note, that creating a separate instance per ingestion operation is an anti-pattern.
+            // IngestClient classes are thread-safe and intended for reuse
+            m_client = KustoIngestFactory.CreateQueuedIngestClient(kustoConnectionStringBuilderDM);
+        }
+        else {
+            Console.WriteLine("no authentication available, set environment variables appKey or cognitoPoolId");
+            
+        }
+
+        }
     }
 
     /// <summary>
@@ -122,28 +142,32 @@ Console.WriteLine ($" resp {response.Token}");
     public async Task FunctionHandler(S3Event evnt, ILambdaContext context)
     {
         Console.WriteLine($"start to ingest");
-        /*
+        
         var awsCredentials = Environment.GetEnvironmentVariable("AwsCredentials");
         string table = Environment.GetEnvironmentVariable("TargetTable");
         string database = Environment.GetEnvironmentVariable("TargetDatabase");
-
-        foreach (var record in evnt.Records)
-        {
-            var s3 = record.S3;
-            Console.WriteLine($"[{record.AwsRegion} - {record.EventTime}] Bucket = {s3.Bucket.Name}, Key = {s3.Object.Key}");
-
-            // Ingest from blobs according to the required properties
-            var kustoIngestionProperties = new KustoQueuedIngestionProperties(databaseName: database, tableName: table)
+        if (evnt.Records != null ){
+            foreach (var record in evnt.Records)
             {
-                FlushImmediately = true
-            };
+                var s3 = record.S3;
+                Console.WriteLine($"[{record.AwsRegion} - {record.EventTime}] Bucket = {s3.Bucket.Name}, Key = {s3.Object.Key}");
 
-            var sourceOptions = new StorageSourceOptions() { DeleteSourceOnSuccess = false, Size = s3.Object.Size };
-            var uri = $"https://{s3.Bucket.Name}.s3.{record.AwsRegion}.amazonaws.com/{s3.Object.Key}";
-            Console.WriteLine($"start to ingest {uri}");
-            await m_client.IngestFromStorageAsync(uri:$"{uri};AwsCredentials={awsCredentials}", ingestionProperties: kustoIngestionProperties, sourceOptions);
-            Console.WriteLine($"complete to ingest {uri}");
+                // Ingest from blobs according to the required properties
+                var kustoIngestionProperties = new KustoQueuedIngestionProperties(databaseName: database, tableName: table)
+                {
+                    FlushImmediately = true
+                };
+
+                var sourceOptions = new StorageSourceOptions() { DeleteSourceOnSuccess = false, Size = s3.Object.Size };
+                var uri = $"https://{s3.Bucket.Name}.s3.{record.AwsRegion}.amazonaws.com/{s3.Object.Key}";
+                Console.WriteLine($"start to ingest {uri}");
+                await m_client.IngestFromStorageAsync(uri:$"{uri};AwsCredentials={awsCredentials}", ingestionProperties: kustoIngestionProperties, sourceOptions);
+                Console.WriteLine($"complete to ingest {uri}");
+            }
         }
-        */
+        else {
+
+            Console.WriteLine("no records in event");
+        }
     }
 }
