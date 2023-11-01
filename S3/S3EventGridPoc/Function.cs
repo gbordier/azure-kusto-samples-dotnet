2cb096e2d1c0a5ca52fd5c996df306d5fca19c92
Guillaume Bordier <g_bordier@hotmail.com>
2023-11-01 17:26:53 +0000

    added lambda coginoto

--- S3/S3EventGridPoc/Function.cs
+++ S3/S3EventGridPoc/Function.cs
@@ -1,7 +1,15 @@
+using Amazon;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.S3Events;
 using Amazon.S3;
 using Amazon.S3.Util;
+
+using Amazon.CognitoIdentity;
+using Amazon.CognitoIdentity.Model;
+
+using System.Collections;
+
+using Microsoft.Identity.Client;
 using Kusto.Data;
 using Kusto.Data.Net.Client;
 using Kusto.Ingest;
@@ -22,13 +30,54 @@ public class Function
     /// </summary>
     public Function()
     {
+        const string poolId = "eu-north-1:80ae676e-949e-4ddb-82cd-a0edc8266fc9";
+        const string clientId= "757a0e90-ab7c-42ff-ac1d-8cf7f5d028d6";
+        const string tenantId = "d1d92357-0fd1-4f9d-853e-a2bf3d687614";
+        string region = Environment.GetEnvironmentVariable("AWS_DEFAULT_REGION");
+        Dictionary<string,string>  Logins= new Dictionary<string,string>();
+        Logins["Azure"]="Adx";
         S3Client = new AmazonS3Client();
+        var cognitoClient = new 	AmazonCognitoIdentityClient( 	 RegionEndpoint.GetBySystemName(region ));
+        var response =  cognitoClient.GetOpenIdTokenForDeveloperIdentityAsync(new GetOpenIdTokenForDeveloperIdentityRequest()
+        {
+            IdentityPoolId = poolId,
+            Logins = Logins
+        }
+        ).Result;
+        
         string appId = Environment.GetEnvironmentVariable("AppId");
         string appKey = Environment.GetEnvironmentVariable("AppKey");
         string authority = Environment.GetEnvironmentVariable("AppTenant");
         string clusterUri = Environment.GetEnvironmentVariable("IngestionUri");
+        
+        // enumerate environment variables
+        var entries = Environment.GetEnvironmentVariables().Cast<DictionaryEntry>()
+            .Select(x => KeyValuePair.Create((string)x.Key, (string)x.Value));
+        var sortedEntries = entries.OrderBy(x => x.Key);
+
+        foreach ( var  v in sortedEntries)
+        {
+            Console.WriteLine("env var : " + v.Key + " = " + v.Value);
+        }
+        Console.WriteLine($"Initializing an ingest client for {clusterUri}");
+//        Console.WriteLine($"cognito client identity is {cognitoClient.}");
+
+Console.WriteLine ($" resp {response.IdentityId}");
+Console.WriteLine ($" resp {response.Token}");
+        if (response.Token.Length > 0){    
+            IConfidentialClientApplication app = ConfidentialClientApplicationBuilder.Create(clientId)
+                .WithClientAssertion(response.Token)      
+                .WithAuthority(AzureCloudInstance.AzurePublic, tenantId)          
+                .Build();
+            var token= app.AcquireTokenForClient(new string[] { $"https://management.core.windows.net/.default" }).ExecuteAsync().Result;
+            if (token != null){
+                Console.WriteLine("AAD token is " + token.AccessToken);
+            }
+        }
+        
         var kustoConnectionStringBuilderDM =
             new KustoConnectionStringBuilder(clusterUri)
+            .With
             .WithAadApplicationKeyAuthentication(appId, appKey, authority);
 
         Console.WriteLine($"Initializing an ingest client for {clusterUri}");
@@ -36,6 +85,7 @@ public class Function
         // Note, that creating a separate instance per ingestion operation is an anti-pattern.
         // IngestClient classes are thread-safe and intended for reuse
         m_client = KustoIngestFactory.CreateQueuedIngestClient(kustoConnectionStringBuilderDM);
+        
     }
 
     /// <summary>
@@ -50,6 +100,8 @@ public class Function
         string appKey = Environment.GetEnvironmentVariable("AppKey");
         string authority = Environment.GetEnvironmentVariable("AppTenant");
         string clusterUri = Environment.GetEnvironmentVariable("IngestionUri");
+        Console.WriteLine($"Initializing an ingest client for {clusterUri}");
+        /*
         var kustoConnectionStringBuilderDM =
             new KustoConnectionStringBuilder(clusterUri)
             .WithAadApplicationKeyAuthentication(appId, appKey, authority);
@@ -59,6 +111,7 @@ public class Function
         // Note, that creating a separate instance per ingestion operation is an anti-pattern.
         // IngestClient classes are thread-safe and intended for reuse
         m_client = KustoIngestFactory.CreateQueuedIngestClient(kustoConnectionStringBuilderDM);
+        */
 
     }
 
@@ -68,6 +121,8 @@ public class Function
     /// </summary>
     public async Task FunctionHandler(S3Event evnt, ILambdaContext context)
     {
+        Console.WriteLine($"start to ingest");
+        /*
         var awsCredentials = Environment.GetEnvironmentVariable("AwsCredentials");
         string table = Environment.GetEnvironmentVariable("TargetTable");
         string database = Environment.GetEnvironmentVariable("TargetDatabase");
@@ -89,5 +144,6 @@ public class Function
             await m_client.IngestFromStorageAsync(uri:$"{uri};AwsCredentials={awsCredentials}", ingestionProperties: kustoIngestionProperties, sourceOptions);
             Console.WriteLine($"complete to ingest {uri}");
         }
+        */
     }
 }
